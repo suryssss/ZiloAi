79213fbf6da4d8d2dd13e7e121914375226f8df9
Rithwik Surya <surya.rithwik2005@gmail.com>
2026-01-16 16:18:19 +0530

    deployment fix

--- src/app/api/stripe/connect/route.ts
+++ src/app/api/stripe/connect/route.ts
@@ -3,17 +3,17 @@ import { currentUser } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET!, {
-  typescript: true,
-  apiVersion: '2024-04-10',
-})
-
 export const dynamic = 'force-dynamic'
 
 export async function GET() {
   try {
     const user = await currentUser()
     if (!user) return new NextResponse('User not authenticated')
+
+    const stripe = new Stripe(process.env.STRIPE_SECRET!, {
+      typescript: true,
+      apiVersion: '2024-04-10',
+    })
 
     const account = await stripe.accounts.create({
       country: 'US',
